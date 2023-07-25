94b1c8388f866ffe6d6a5a3b2ef0a89fb8a325b1
Sodiq Farhan A <sodiqfarhan360@gmail.com>
2023-07-25 11:26:40 +0100

    Add cors module

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const connectDB = require("./config/dbConfig");
 
